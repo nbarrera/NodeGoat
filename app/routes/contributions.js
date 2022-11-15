650a3285bcedecfec3635b3d6be060381fce56d4
Nicolas Barrera <nbarrera@gmail.com>
2022-11-14 21:22:28 -0300

    Fixes for A1:2010 / A3:2021 Injection
    validate input on allocations, it needs to be a number avoid injection
    
    avoid use of eval, validate input correctly, it should be a number
    avoids injection
    
    Use encoders when logging arguments to avoid injection

--- app/routes/contributions.js
+++ app/routes/contributions.js
@@ -27,18 +27,10 @@ function ContributionsHandler(db) {
 
     this.handleContributionsUpdate = (req, res, next) => {
 
-        /*jslint evil: true */
-        // Insecure use of eval() to parse inputs
-        const preTax = eval(req.body.preTax);
-        const afterTax = eval(req.body.afterTax);
-        const roth = eval(req.body.roth);
-
-        /*
-        //Fix for A1 -1 SSJS Injection attacks - uses alternate method to eval
+        //Fix for: A1:2010 -1 SSJS Injection attacks A3:2021 Injection - uses alternate method to eval
         const preTax = parseInt(req.body.preTax);
         const afterTax = parseInt(req.body.afterTax);
         const roth = parseInt(req.body.roth);
-        */
         const {
             userId
         } = req.session;
